bbe3efd90395dd802993bd31dad75afa91a2370b
장규범 <jkb2221@gmail.com>
2021-03-10 20:18:05 +0900

    Update script.py

--- CodeCademy/script.py
+++ CodeCademy/script.py
@@ -1,1 +1,7 @@
 print("Hello "+ "Kyubum!")
+
+# 오류 변경
+# print("How do you make a hot dog stand?')
+print("How do you make a hot dog stand?")
+#print(You take away its chair!)
+print("You take away its chair!")
