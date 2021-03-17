cf244404b799df7ba3c2a14513bd5514cff0e870
장규범 <jkb2221@gmail.com>
2021-03-17 21:17:17 +0900

    Update battleship.py

--- CodeCademy/battleship.py
+++ CodeCademy/battleship.py
@@ -7,7 +7,7 @@ for x in range(0, 5):
 
 def print_board(board):
   for row in board:
-    print " ".join(row)
+    print(" ".join(row))
 
 print_board(board)
 
@@ -19,28 +19,28 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-print ship_row
-print ship_col
+print (ship_row)
+print (ship_col)
 
 # Everything from here on should be in your for loop
 # don't forget to properly indent!
 for turn in range(4):
-  print "Turn", turn + 1
-  guess_row = int(raw_input("Guess Row: "))
-  guess_col = int(raw_input("Guess Col: "))
+  print ("Turn", turn + 1)
+  guess_row = int(input("Guess Row: "))
+  guess_col = int(input("Guess Col: "))
 
   if guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sank my battleship!"
+    print("Congratulations! You sank my battleship!") 
     break
   else:
     if guess_row not in range(5) or \
       guess_col not in range(5):
-      print "Oops, that's not even in the ocean."
+      print ("Oops, that's not even in the ocean.")
     elif board[guess_row][guess_col] == "X":
       print( "You guessed that one already." )
     else:
-      print "You missed my battleship!"
+      print ("You missed my battleship!")
       board[guess_row][guess_col] = "X"
     if (turn == 3):
-      print "Game Over"
+      print ("Game Over")
     print_board(board)
